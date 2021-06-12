8ae26194c37b61ea6d713760f815ec6c348ca5a6
fickrybiliman <bil.iman.fickry@gmail.com>
2021-06-12 12:48:20 +0700

    remove default file from create-react-app & add components and styles component

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Topbar from "./components/topbar/Topbar"
+
+function App() {
+  return (
+    <div className="App">
+      <Topbar />
+      <div className="section">
+        
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/portofolio/Portofolio.jsx
@@ -0,0 +1,9 @@
+import "./portofolio.scss"
+
+export default function Portofolio() {
+    return (
+        <div className="portofolio">
+            
+        </div>
+    )
+}
